82e4af9731a47ace309234ef7926f490890a9135
Katiane <126177049+Katianefatec@users.noreply.github.com>
2023-05-16 23:05:23 -0300

    Inclui o arquivo app.py

---
+++ Src/app.py
@@ -0,0 +1,23 @@
+from flask import app, Flask, render_template, request, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/experiencia')
+def experiencia():
+    return render_template('experiencia.html')
+
+@app.route('/sobremim')
+def sobremim():
+    return render_template('sobremim.html') 
+
+@app.route('/teste')
+def teste():
+    return render_template('teste.html') 
+
+if __name__ == '__main__':  
+    app.run('0.0.0.0')        
