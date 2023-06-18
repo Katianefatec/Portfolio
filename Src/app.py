e344a4ee9c46830e7204976e897f7b84c8dcf387
Katiane <katiane.soares@fatec.sp.gov.br>
2023-06-17 22:11:48 -0300

    inclui passagem de argumentos

--- Src/app.py
+++ Src/app.py
@@ -9,39 +9,22 @@ def home():
 
 @app.route('/experiencia')
 def experiencia():
-    return render_template('experiencia.html')
+    experiencias = [
+        {"imagem": "../static/img/fisio.jpg", "alt": "formacao", "titulo": "Formada em fisioterapia pela FCT - UNESP (2001-2004)"},
+        {"imagem": "../static/img/faculeste.jpeg", "alt": "cargo2", "titulo": "Pós-graduada em Intervenção psicológica na mediação e resolução de conflitos (2022-2023)"},
+        {"imagem": "../static/img/fatec.jpeg", "alt": "cargo5", "titulo": "Cursando Desenvolvimento de Software Multiplataforma"}
+    ]
+    
+    experiencias2 = [
+        {"imagem": "../static/img/nossacaixa.png", "alt": "cargo3", "titulo": "Auxiliar administrativo do Banco Nossa Caixa (2006-2007)"},
+        {"imagem": "../static/img/tjsp.jpg", "alt": "cargo1", "titulo": "Escrevente técnica do Tribunal de Justiça do Estado de São Paulo (2007-2009)"},
+        {"imagem": "../static/img/jfsp.jpg", "alt": "cargo4", "titulo": "Técnica judiciária da Justiça Federal de São Paulo (2009 até o momento)"}
+    ]
+    return render_template('experiencia.html', experiencias=experiencias, experiencias2=experiencias2)
 
 @app.route('/sobremim')
 def sobremim():
-    return render_template('sobremim.html', experiencias=experiencias) 
-
-experiencias = [
-    {
-        'imagem': '../static/img/fisio.jpg',
-        'cargo': 'Formada em fisioterapia pela FCT - UNESP (2001-2004)'
-    },
-    {
-        'imagem': '../static/img/faculeste.jpeg',
-        'cargo': 'Pós-graduada em Intervenção psicológica na mediação e resolução de conflitos (2022-2023)'
-    },
-    {
-        'imagem': '../static/img/fatec.jpeg',
-        'cargo': 'Cursando Desenvolvimento de Software Multiplataforma'
-    },
-    {
-        'imagem': '../static/img/nossacaixa.png',
-        'cargo': 'Auxiliar administrativo do Banco Nossa Caixa (2006-2007)'
-    },
-    {
-        'imagem': '../static/img/tjsp.jpg',
-        'cargo': 'Escrevente técnica do Tribunal de Justiça do Estado de São Paulo (2007-2009)'
-    },
-    {
-        'imagem': '../static/img/jfsp.jpg',
-        'cargo': 'Técnica judiciária da Justiça Federal de São Paulo (2009 até o momento)'
-    }
-]
-
+    return render_template('sobremim.html') 
 
 if __name__ == '__main__':  
     app.run('0.0.0.0')        
