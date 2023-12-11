1c326210c97a5777630c51ab0fe295d15a501bd1
Katiane <katiane.soares@fatec.sp.gov.br>
2023-12-11 11:56:06 -0300

    ajustes no layout

--- Src/app.py
+++ Src/app.py
@@ -13,14 +13,16 @@ def experiencia():
         {"imagem": "../static/img/fisio.jpg", "alt": "formacao", "titulo": "Formada em fisioterapia pela FCT - UNESP (2001-2004)"},
         {"imagem": "../static/img/faculeste.jpeg", "alt": "cargo2", "titulo": "Pós-graduada em Intervenção psicológica na mediação e resolução de conflitos (2022-2023)"},
         {"imagem": "../static/img/fatec.jpeg", "alt": "cargo5", "titulo": "Cursando Desenvolvimento de Software Multiplataforma"}
-    ]
-    
+    ]   
+   
     experiencias2 = [
         {"imagem": "../static/img/nossacaixa.png", "alt": "cargo3", "titulo": "Auxiliar administrativo do Banco Nossa Caixa (2006-2007)"},
         {"imagem": "../static/img/tjsp.jpg", "alt": "cargo1", "titulo": "Escrevente técnica do Tribunal de Justiça do Estado de São Paulo (2007-2009)"},
         {"imagem": "../static/img/jfsp.jpg", "alt": "cargo4", "titulo": "Técnica judiciária da Justiça Federal de São Paulo (2009 até o momento)"}
     ]
     return render_template('experiencia.html', experiencias=experiencias, experiencias2=experiencias2)
+
+
 
 @app.route('/sobremim')
 def sobremim():
